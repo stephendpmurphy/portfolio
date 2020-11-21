fb699920d68909873327273a6c1e3a28760aec77
stephendpmurphy <stephendpmurphy@msn.com>
2020-11-20 21:05:30 -0500

    Adding NodeJS stuff to hopefully make this shit work

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const confetti = require("canvas-confetti")
+
+var myCanvas = document.getElementById('myCanvas');
+
+var myConfetti = confetti.create(myCanvas, {
+  resize: true,
+  useWorker: true
+});
+myConfetti({
+  particleCount: 100,
+  spread: 160
+  // any other options from the global
+  // confetti function
+});
