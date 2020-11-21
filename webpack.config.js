ad2806222d016a961377517f9132f145d883618e
stephendpmurphy <stephendpmurphy@msn.com>
2020-11-20 21:22:32 -0500

    Adding proc file for serving up our compiled site

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
         filename: 'js/bundle.js'
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: './public',
     },
     plugins: [
         new htmlWebpackPlugin({
